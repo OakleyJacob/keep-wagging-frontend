f390eca98a2862cf5efacd6fc257c5cd02eb264c
miguelosvaldo <miguel.o.molina@icloud.com>
2023-07-19 09:36:33 -0700

    frontend structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
+import { Routes, Route } from "react-router-dom"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import DogEdit from "./pages/DogEdit"
+import DogIndex from "./pages/DogIndex"
+import DogNew from "./pages/DogNew"
+import DogShow from "./pages/DogShow"
+import Home from "./pages/Home"
+import NotFound from "./pages/NotFound"
+import AboutUs from "./pages/AboutUs"
+import SignUp from "./pages/SignUp"
+import SignIn from "./pages/SignIn"
+import { mockDog } from './assets/MockDog'
 
-function App() {
+
+const App = () => {
+
+  const [dogs, setDogs] = useState(mockDog)
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/dogindex" element={<DogIndex />} />
+      <Route path="/dogshow" element={<DogShow />} />
+      <Route path="/dognew" element={<DogNew />} />
+      <Route path="/dogedit" element={<DogEdit />} />
+      <Route path="/aboutus" element={<AboutUs />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+    <Footer />
+  </>
 
-export default App;
+)}
+
+export default App
+
+
+
+
