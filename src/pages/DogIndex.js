e4902ced7bb7704fc63662839d04626449fe8e47
Phalco1 <elphalco1@gmail.com>
2023-07-21 16:05:53 -0500

    remove comments

--- src/pages/DogIndex.js
+++ src/pages/DogIndex.js
@@ -2,29 +2,6 @@ import React from 'react'
 import { Card, CardBody, CardSubtitle, CardTitle } from 'reactstrap'
 import { NavLink } from 'react-router-dom'
 
-// const DogIndex = (props) => {
-//   const navigate = useNavigate()
-//   const goToShow = (id) => {
-//     navigate('/dogshow/'+ id)
-//   }
-//   return (
-//     <>
-//       <ul className='dog-cards'>
-//         {props.dogs?.map((value, index) => {
-//          return  <li  key = {index} onClick = {() => goToShow(index+1)}>     
-//               <h1>name: {value.name}</h1>
-//               <h2>age: {value.age}</h2>
-//               <h3>vaccinated: {value.vaccination_status?'yes':'no'}</h3>
-//               <p>description: {value.description}</p>
-//               <img src = {value.image} alt = {'photo of '+ value.name}></img>
-//           </li>
-//         })}
-        
-//       </ul>
-//     </>
-//   )
-// }
-
 const DogIndex = ({dogs}) => {
   return (
     <div className='dog-cards'>
